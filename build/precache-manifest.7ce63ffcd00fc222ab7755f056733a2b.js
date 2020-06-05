439264d2579831e6429836a4333392d174563652
Song-Fang <sof10@pitt.edu>
2020-06-05 03:39:42 -0400

    fix bugs

---
+++ build/precache-manifest.7ce63ffcd00fc222ab7755f056733a2b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2eb5093ac7e8e58684b25047aa471e0",
+    "url": "/Personal-Page/index.html"
+  },
+  {
+    "revision": "dd1e67442b69f58b6536",
+    "url": "/Personal-Page/static/css/main.99658e7c.chunk.css"
+  },
+  {
+    "revision": "06141eb1a48297e23252",
+    "url": "/Personal-Page/static/js/2.5f731b6d.chunk.js"
+  },
+  {
+    "revision": "c97f78f18c86ebca6e481af90f3b6b2a",
+    "url": "/Personal-Page/static/js/2.5f731b6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1e67442b69f58b6536",
+    "url": "/Personal-Page/static/js/main.b8badbb2.chunk.js"
+  },
+  {
+    "revision": "b0cbc14e8fade70338f1",
+    "url": "/Personal-Page/static/js/runtime-main.b2194ece.js"
+  },
+  {
+    "revision": "522ff665b0791c5cd808328a7c3c5329",
+    "url": "/Personal-Page/static/media/HIT.522ff665.gif"
+  },
+  {
+    "revision": "c92318e968991d68800862dcf2585ca4",
+    "url": "/Personal-Page/static/media/PITT.c92318e9.png"
+  },
+  {
+    "revision": "6418195d3040b311b8d2c51553ecd40a",
+    "url": "/Personal-Page/static/media/Profile1.6418195d.jpg"
+  },
+  {
+    "revision": "d98fd057213fae163d0cdba34e0bfc10",
+    "url": "/Personal-Page/static/media/Profile2.d98fd057.jpg"
+  },
+  {
+    "revision": "7b3272a1a93c3c3ec1056e8be3295208",
+    "url": "/Personal-Page/static/media/Quora.7b3272a1.png"
+  },
+  {
+    "revision": "c901709751f13d8501ca9bca3b7c7f54",
+    "url": "/Personal-Page/static/media/SongFang.c9017097.jpg"
+  },
+  {
+    "revision": "626953c1b0fcc52dbbc475f55820d030",
+    "url": "/Personal-Page/static/media/aws.626953c1.png"
+  },
+  {
+    "revision": "50dc777038c8d35f095aab19feed3550",
+    "url": "/Personal-Page/static/media/backImage.50dc7770.png"
+  },
+  {
+    "revision": "863addebeaa9d9090fbad803f1c93a4f",
+    "url": "/Personal-Page/static/media/email.863addeb.png"
+  },
+  {
+    "revision": "07ea0bede6156d7f2625b3cbe55c0abc",
+    "url": "/Personal-Page/static/media/github.07ea0bed.png"
+  },
+  {
+    "revision": "530b6f98d3157b1b80901b3588c2b1aa",
+    "url": "/Personal-Page/static/media/java.530b6f98.png"
+  },
+  {
+    "revision": "d552a8f8e2e5b31381ff2d576cd07b80",
+    "url": "/Personal-Page/static/media/javascript.d552a8f8.png"
+  },
+  {
+    "revision": "e068a7fe43fbf17c16de64f26c5e7ccc",
+    "url": "/Personal-Page/static/media/js.e068a7fe.png"
+  },
+  {
+    "revision": "70353f9adaf6d77a9b4dc16f3d06054b",
+    "url": "/Personal-Page/static/media/mongodb.70353f9a.png"
+  },
+  {
+    "revision": "adaf877377ac7b40a30c5f82ea7dbd07",
+    "url": "/Personal-Page/static/media/mysql.adaf8773.png"
+  },
+  {
+    "revision": "2ac89ae41df3f4bad20c2ecd20b9b25b",
+    "url": "/Personal-Page/static/media/neo4j.2ac89ae4.png"
+  },
+  {
+    "revision": "5d90faf9af5d5acf8fbe96caeb4a9dcd",
+    "url": "/Personal-Page/static/media/phone.5d90faf9.png"
+  },
+  {
+    "revision": "2e3aebfa6f34abbb75e45658eea48336",
+    "url": "/Personal-Page/static/media/react.2e3aebfa.png"
+  },
+  {
+    "revision": "423b9c4183460264958fc7f3b90c8266",
+    "url": "/Personal-Page/static/media/redux.423b9c41.jpg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <div className="App">
           <div>
-             <Route exact path='/' component={Home}/>
+             <Home/>
           </div>
       </div>
     );
